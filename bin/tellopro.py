03172e67b8bea01bef52c8e24d2f13c496a50508
21700616 <21700616@handong.edu>
2019-11-24 23:56:47 +0900

    Initial commit

--- bin/tellopro.py
+++ bin/tellopro.py
@@ -7,8 +7,16 @@ import TelloPro
 tello = Tello()
 
 command_lst = []
-command_lst.append(TelloPro.get_instance('takeoff'))
-command_lst.append(TelloPro.get_instance('land'))
+command_lst.append(TelloPro.get_instance('takeoff', -1))
+command_lst.append(TelloPro.get_instance('land', 50))
+command_lst.append(TelloPro.get_instance('up', 20))
+command_lst.append(TelloPro.get_instance('down', 20))
+command_lst.append(TelloPro.get_instance('left', 20))
+command_lst.append(TelloPro.get_instance('right', 20))
+command_lst.append(TelloPro.get_instance('forward', 20))
+command_lst.append(TelloPro.get_instance('back', 20))
+command_lst.append(TelloPro.get_instance('cw', 20))
+command_lst.append(TelloPro.get_instance('ccw', 20))
 
 for command in command_lst:
 	tello.send_command_instance(command)
